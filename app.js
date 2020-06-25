f91ebded64ce00f99e60e2af7f75f87f793d2934
shovancak <s.hovancak@gmail.com>
2020-06-25 15:43:52 +0200

    Error handling for unsupported routes

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
 
+const HttpError = require("./models/http-error");
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("This page does not exist.", 404);
+  throw error;
+});
 
 // error handling function
 app.use((error, req, res, nest) => {
