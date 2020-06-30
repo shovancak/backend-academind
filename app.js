25596c04f568cb6a71984f52dd3982471bfa520e
shovancak <s.hovancak@gmail.com>
2020-06-30 15:54:00 +0200

    Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const usersRouters = require("./routes/users-routes");
 const placesRoutes = require("./routes/places-routes");
@@ -28,5 +29,15 @@ app.use((error, req, res, nest) => {
     .json({ message: error.message || "An unknown error occurred." });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb+srv://samuel:83461834Sh@cluster0-eamri.mongodb.net/places?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 console.log("Server is running.");
