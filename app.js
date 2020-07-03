027bd06b51138a7dca83425e28251a5c7f2d4fe0
shovancak <s.hovancak@gmail.com>
2020-07-03 15:32:05 +0200

    Handling CORS errors

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/users", usersRouters);
 app.use("/api/places", placesRoutes);
 
@@ -31,7 +41,7 @@ app.use((error, req, res, nest) => {
 
 mongoose
   .connect(
-    "mongodb+srv://samuel:83461834Sh@cluster0-eamri.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://samuel:83461834Sh@cluster0-eamri.mongodb.net/mern-academind?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(5000);
