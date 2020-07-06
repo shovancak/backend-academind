3203eb49f749d6f638909f697551e4daaeb13232
shovancak <s.hovancak@gmail.com>
2020-07-06 18:18:54 +0200

    fixing nameing error

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 
-const placesControllers = require("../controllers/places-controllers");
+const placesControllers = require("../controllers/places-controller");
 
 const router = express.Router();
 
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 
-const usersController = require("../controllers/users-controllers");
+const usersController = require("../controllers/users-controller");
 
 const router = express.Router();
 
