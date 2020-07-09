b05bc330f6d53f845fd3bb740e92635525bc1b76
shovancak <s.hovancak@gmail.com>
2020-07-09 13:51:01 +0200

    Uploading images for new place

--- util/location.js
+++ util/location.js
@@ -1,14 +1,10 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const HttpError = require('../models/http-error');
+const HttpError = require("../models/http-error");
 
-const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
+const API_KEY = "AIzaSyA9gCSilKD19Az6aoz9DlasYYE9GgY9O_I";
 
 async function getCoordsForAddress(address) {
-  // return {
-  //   lat: 40.7484474,
-  //   lng: -73.9871516
-  // };
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
@@ -17,9 +13,9 @@ async function getCoordsForAddress(address) {
 
   const data = response.data;
 
-  if (!data || data.status === 'ZERO_RESULTS') {
+  if (!data || data.status === "ZERO_RESULTS") {
     const error = new HttpError(
-      'Could not find location for the specified address.',
+      "Could not find location for the specified address.",
       422
     );
     throw error;
