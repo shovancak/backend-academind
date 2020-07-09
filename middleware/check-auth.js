289f9847c502af5f376996e819f03c40b3aa02c6
shovancak <s.hovancak@gmail.com>
2020-07-09 19:36:01 +0200

    Avoiding error made by browsers OPTIONS request

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next(); // allowing OPTIONS request created by browser to pass/ dont block actual request send by user
+  }
   try {
     const token = req.headers.authorization.split(" ")[1]; //Authorization: "Bearer TOKEN"
     if (!token) {
